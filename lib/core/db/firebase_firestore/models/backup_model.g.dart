c2a751c4db49c768fc73bd5a400662ac797f18ba
plotnikov228 <plotnikofffromanchik@mail.ru>
2023-06-24 16:48:59 +0300

    fix auth, and recovery data

---
+++ lib/core/db/firebase_firestore/models/backup_model.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'backup_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BackupModel _$BackupModelFromJson(Map<String, dynamic> json) => BackupModel(
+      json['file_ID'] as String,
+      json['date_time'] as String,
+      json['record'] as String,
+      json['service'] as String,
+      json['file_name'] as String,
+    );
+
+Map<String, dynamic> _$BackupModelToJson(BackupModel instance) =>
+    <String, dynamic>{
+      'service': instance.service,
+      'file_ID': instance.file_ID,
+      'date_time': instance.date_time,
+      'record': instance.record,
+      'file_name':instance.file_name
+    };
